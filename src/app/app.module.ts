b56b93fbdeee37b99d1e0c469fe00302aba344eb
brleinad <daniel@danielrb.dev>
2021-02-17 18:55:42 -0500

    add basic layout
    - added city selector component
    - added forecast display component
    - basic app layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CitySelectorComponent } from './components/city-selector/city-selector.component';
+import { ForecastDisplayComponent } from './components/forecast-display/forecast-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitySelectorComponent,
+    ForecastDisplayComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/forecast-display/forecast-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-display',
+  templateUrl: './forecast-display.component.html',
+  styleUrls: ['./forecast-display.component.scss']
+})
+export class ForecastDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
